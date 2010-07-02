e53037f5806af513d377ec266ac029fea5178aee
Erik van de Pol <erikvandepol@b3partners.nl>
2010-07-02 13:15:05 +0000

    Geotools update naar trunk (2.6.4 op het moment)

--- src/nl/b3p/geotools/data/suf2/SUF2DataStoreFactory.java
+++ src/nl/b3p/geotools/data/suf2/SUF2DataStoreFactory.java
@@ -9,6 +9,7 @@ import java.util.HashMap;
 import java.util.Map;
 import org.geotools.data.DataStore;
 import org.geotools.data.DataStoreFactorySpi;
+import org.geotools.data.FileDataStore;
 import org.geotools.data.FileDataStoreFactorySpi;
 import org.geotools.referencing.CRS;
 import org.opengis.referencing.FactoryException;
@@ -96,7 +97,7 @@ public class SUF2DataStoreFactory implements FileDataStoreFactorySpi {
         return SUF2DataStore.getURLTypeName(url);
     }
 
-    public DataStore createDataStore(URL url) throws IOException {
+    public FileDataStore createDataStore(URL url) throws IOException {
         Map params = new HashMap();
         params.put(PARAM_URL.key, url);
 
@@ -108,7 +109,7 @@ public class SUF2DataStoreFactory implements FileDataStoreFactorySpi {
         }
     }
 
-    public DataStore createDataStore(Map params) throws IOException {
+    public FileDataStore createDataStore(Map params) throws IOException {
         if (!canProcess(params)) {
             throw new FileNotFoundException("SUF2 file not found: " + params);
         }
